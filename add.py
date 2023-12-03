676f28df581bbc3ef56d41ff35b9dcef3739b38c
Ishwor <dulalishan3@gmail.com>
2023-12-03 15:37:47 +0545

    add an addition script without the use of  oeprator

---
+++ add.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number:\n>> "))
+num2 = int(input("Enter another number:\n>> "))
+lst = []
+for _ in range(num1):
+    lst.append("a")
+for _ in range(num2):
+    lst.append("a")
+print("the sum is:",len(lst))
